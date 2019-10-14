2ab8c6f4f68bddfdbdcc696c96e49099c8f2a6b5
Viacheslav Zhygulin <s.zhygulin@gmail.com>
2019-10-14 12:30:39 -0400

    added first file

---
+++ estimation.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import random
+from statistics import mean
+import operator
+import matplotlib.pyplot as plt
+
+# run single experiment estimating block propagation time
+
+def estimate_block_propagation_time(n,m):
+    # generate graph and compute shortest paths lenghts from node 0 to others
+    graph = nx.barabasi_albert_graph(n,m)
+    for (u, v, w) in graph.edges(data=True):
+        w['weight'] = random.uniform(0.08, 16)
+        #print(graph.edges[u,v]['weight'])
+    shortest_paths = nx.single_source_dijkstra_path_length(graph, 0)
+    return max(shortest_paths.items(), key=operator.itemgetter(1))[1]
+
+#parameter m is how much connection committee member has
+m = 3
+
+# k is amount of experiments for averaging
+k = 100
+
+ydata = []
+xdata = range(20,200)
+for n in xdata:
+    data = [estimate_block_propagation_time(n,m) for _ in range(k)]
+    #print(n, mean(data))
+    ydata.append(mean(data))
+
+plt.plot(xdata, ydata)
+plt.ylabel("Propagation Time, Seconds")
+plt.xlabel("Committee Size")
+plt.show()
+
